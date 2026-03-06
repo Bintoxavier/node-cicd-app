2046f517d7caf82982752d80272434e63bbeac59
Bintoxavier <bintoxavier7@gmail.com>
2026-03-06 15:58:02 +0000

    test webhook-3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('padichu monne njn scna 🚀');
+    res.send('padichu monne njn scna. 🚀');
 });
 
 app.listen(3000, () => {
